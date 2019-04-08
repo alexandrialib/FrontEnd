bf2c2716daf508fc77aa264ba1c56c3160e29cb2
YohannaToro <yoha_duran156@hotmail.com>
2019-04-07 22:32:16 -0500

    test

--- src/js/Components/Category.js
+++ src/js/Components/Category.js
@@ -50,33 +50,3 @@ export default class Category extends Component{
 
 }
 
-/*
-
-<div class="badge blue">
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="216px" height="232px" viewBox="0 0 216 232">
-		<path fill="#2B2B2B" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169    c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z"/>
-</svg>
-  <p class="title">CRYPTO MASTER</p>
-  <p class="subtitle">CTF 365</p>
-</div>
-*/
-/* render () {
-        const {title, img, instructions} = this.props;
-        const ingredients = this.props.ingredients.map((ing, index) => (
-                <li key={index}>{ing}</li>
-            ));
-        return (
-            <div className="recipe-card">
-                <div className="recipe-card-image">
-                    <img src={img} alt={title} />
-                </div>
-                <div className="recipe-card-content">
-                    <h2 className="recipe-title">{title}</h2>
-                    <h2>Category:</h2>
-                    <ul>{ingredients}</ul>
-                    <h2>Description:</h2>
-                    <p>{instructions}</p>
-                </div>
-            </div>
-        );
-    */
--- src/js/DataProvider.js
+++ src/js/DataProvider.js
@@ -1,14 +1,22 @@
 import axios from "axios";
 export const url= "http://alexandria-lib-back.herokuapp.com/";
 export async function getAllCategories(name){
-    console.log('entre');
+   // console.log('entre');
     const {data:posts}= await axios.get(url+name);
   return posts;
 
 }
 export async function getCategoryByName(name){
-    console.log('entre');
-    const {data:posts}= await axios.get(url+"/categories/"+name).then(res=>res.title);
+   // console.log('entre aqui');
+    const {data:posts}= await axios.get("http://alexandria-lib-back.herokuapp.com/categories/"+name);
+    //console.log("holi");
   return posts;
+
+}
+export async function getArticleFromCategory(category,title){
+  //console.log(category);console.log(title);
+  const {data:posts}= await axios.get("http://alexandria-lib-back.herokuapp.com/categories/"+category+"/articles/"+title);
+  //console.log("holi");
+return posts;
 
 }
