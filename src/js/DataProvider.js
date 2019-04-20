d125b9b1e40d2196a4fb3f5dfdd627d7c5cbf2bc
YohannaToro <yoha_duran156@hotmail.com>
2019-04-19 23:27:51 -0500

    some changes

--- src/js/DataProvider.js
+++ src/js/DataProvider.js
@@ -1,45 +1,41 @@
 import axios from "axios";
 axios.defaults.baseURL = "https://alexandria-lib-back.herokuapp.com";
-export async function getAllCategories(name) {
-  // console.log('entre');
-  const { data: posts } = await axios.get(name);
+export async function getAllCategories(name){
+   // console.log('entre');
+    const {data:posts}= await axios.get(url+name);
   return posts;
+
 }
-export async function getCategoryByName(name) {
-  // console.log('entre aqui');
-  const { data: posts } = await axios.get("/categories/" + name);
-  //console.log("holi");
+export async function getCategoryByName(name){
+   // console.log('entre aqui');
+    const {data:posts}= await axios.get("/categories/"+name);
+    //console.log("holi");
   return posts;
+
 }
-<<<<<<< HEAD
 export async function getArticleFromCategory(category,title){
  
-  const {data:posts}= await axios.get("http://alexandria-lib-back.herokuapp.com/categories/"+category+"/articles/"+title);
+  const {data:posts}= await axios.get("/categories/"+category+"/articles/"+title);
 
 return posts;
 
 }
 export async function getAllCommentsByArticle(title){
-  const {data:posts}= await axios.get("https://alexandria-lib-back.herokuapp.com/categories/Sort/articles/Quick/comments");
+  const {data:posts}= await axios.get("/categories/Sort/articles/Quick/comments");
   return posts;
 
 }
-export async function postCommentInArticle(){
-  
-  axios.post('https://alexandria-lib-back.herokuapp.com/categories/Sort/articles/Quick/comments', {
-    author: {"id":"5cba6fbc9d93ea0004d2bd2e","username":"admin","email":null,"name":null,"lastName":null},
-    content: 'Flintstone',
-    date:["1","1","1","1","1","1"],
-    likes:[]
+export async function postCommentInArticle(user,comment,success,fail){
+  axios({
+    method: "post",
+    url: "/categories/Sort/articles/Quick/comments",
+    auth: user,
+    data: comment
+
   })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  .then(success())
+  .catch(fail());
 }
-=======
 export async function getArticleFromCategory(category, title) {
   //console.log(category);console.log(title);
   const { data: posts } = await axios.get(
@@ -67,4 +63,3 @@ export function getUser(user, success, fail) {
     .then(success)
     .catch(fail);
 }
->>>>>>> b4b5e74c31669ecb6e450c1b9257f04ebb36efee
